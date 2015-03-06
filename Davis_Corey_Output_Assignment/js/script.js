78d217f07f87ec9a599c30d8976797ddb32aeeff
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-06 00:59:48 -0500

    Assignment of Variables
    Number, String, and Boolean variables assigned to declared variables

--- Davis_Corey_Output_Assignment/js/script.js
+++ Davis_Corey_Output_Assignment/js/script.js
@@ -9,6 +9,15 @@ var favoriteFood; //Declaration of variable
 var familyLearning; //Declaration of variable
 var learningType;  //Declaration of variable
 
+numberOfChildren = 3  // Assigning a number variable
+myGirls = 2 // Assigning a number variable
+myBoys = 1 // Assigning a number variable
+favoriteFood = "Pizza"; // Assigning a string variable
+familyLearning = true; // Assigning a boolean variable
+learningType = "Visual"; // Assigning a string variable
+
+
+
 
 
 
