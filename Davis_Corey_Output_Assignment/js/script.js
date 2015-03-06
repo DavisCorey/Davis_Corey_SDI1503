433daf84abd2e46baee783936be435dd004cc999
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-06 17:15:36 -0500

    Corrected JSLint
    JSLint errors corrected

--- Davis_Corey_Output_Assignment/js/script.js
+++ Davis_Corey_Output_Assignment/js/script.js
@@ -9,15 +9,15 @@ var favoriteFood; //Declaration of variable
 var familyLearning; //Declaration of variable
 var learningType;  //Declaration of variable
 
-numberOfKids = 3  // Assigning a number variable
-myGirls = 2 // Assigning a number variable
+numberOfKids = 3;  // Assigning a number variable
+myGirls = 2; // Assigning a number variable
 favoriteFood = "pizza"; // Assigning a string variable
 familyLearning = true; // Assigning a boolean variable
 learningType = "visual"; // Assigning a string variable
 
 //Output of variables and independent strings within console.
-console.log("I have",numberOfKids,"children. ","My", myGirls,"girls love technology and",favoriteFood,"as much as I do. "
-,"I learn best with",learningType,"reminders of my accomplishments, this helps me stay",familyLearning,"to my daily obligations.");
+console.log("I have", numberOfKids, "children. ", "My", myGirls, "girls love technology and", favoriteFood, "as much as I do. "
+, "I learn best with",learningType,"reminders of my accomplishments, this helps me stay", familyLearning, "to my daily obligations.");
 
 
 alert("For " + learningType + " confirmation of output of variables please go to the Console.")// Alert output of variable with string concatenated.
--- Video_Activities/Values_Variables/js/script.js
+++ Video_Activities/Values_Variables/js/script.js
@@ -10,7 +10,10 @@ console.log("Hello World!");
  console.log(yearBorn);
  */
 
-alert(yearBorn);
+//window.prompt("sometext", "defaultText");
+
+
+//alert(yearBorn);
 
 //Defining Variables
 
@@ -21,7 +24,7 @@ var yearOfBirth = 1970; //declaring and defining on the same line
 var age;
 age = 38;
 
-alert(age, yearBorn);
+//alert(age, yearBorn);
 
 var isStudent = false;  //Boolean Variable
 
@@ -31,9 +34,19 @@ var phrase = 'I don\'t know'; //Line break in code
 
 //Arrays
 
-var muppetNames = ["Kermit","Miss Piggy"];  //0,1,2,3
+var muppetNames = ["Kermit", "Miss Piggy"];  //0,1,2,3
 
 muppetNames[1] = "Rizzo"; //changes the index #1 to Rizzo
 muppetNames[2] = "Fozzy";
 
-console.log(muppetNames[1], isStudent, phrase," who is ", firstName);  //Lets me see whats in the index
+
+
+console.log(muppetNames[1], isStudent, phrase, " who is ", firstName);  //Lets me see whats in the index
+
+/*var person = prompt("Please enter your name", "Harry Potter");
+
+if (person !== null) {
+    document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+}
+*/
