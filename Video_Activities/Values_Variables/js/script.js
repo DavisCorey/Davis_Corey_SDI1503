c428c62cf242a5fa6640814d0f4be48f1a9a76f5
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-05 23:48:11 -0500

    Boolean variable
    Line break in code and boolean variable

--- Video_Activities/Values_Variables/js/script.js
+++ Video_Activities/Values_Variables/js/script.js
@@ -21,4 +21,10 @@ var yearOfBirth = 1970; //declaring and defining on the same line
 var age;
 age = 38;
 
-alert(age, yearBorn)
+alert(age, yearBorn);
+
+var isStudent = false;  //Boolean Variable
+
+var firstName = "Kermit"; //String Variable
+
+var phrase = 'I don\'t know'; //Line break in code
