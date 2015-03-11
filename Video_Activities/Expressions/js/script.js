35858673c8849b851b909a8c5fdb7531750c6cbd
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-10 22:00:09 -0400

    Variable Addition
    Declaring a variable before using an operator.

--- Video_Activities/Expressions/js/script.js
+++ Video_Activities/Expressions/js/script.js
@@ -1,6 +1,7 @@
 //Expressions
 
 var a = 2;  //Declare and assigning value of 2
+a = a + 3; //add 3 to variable 'a', must declare variable before operator usage
 console.log(a);  //Confirming the value of variable 'a' in the console
 
 
