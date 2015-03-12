2a355df24cf5ac23572d279900e9e433e59db835
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-11 22:41:33 -0400

    Number to String
    Conversion of number variables to string to be concated.

--- Video_Activities/Casting/js/script.js
+++ Video_Activities/Casting/js/script.js
@@ -5,5 +5,11 @@
 
 var stringVar = "6";
 var result = 7 + Number(stringVar);   //Variable declaration, string conversion to number value
-console.log(result);
+//console.log(result);
 
+var areaCode = 407;  //Variable Declaration and assignment
+var firstPart = 203;  //Variable Declaration and assignment
+var secPart = 4529;  //Variable Declaration and assignment
+
+var phoneNo = String(areaCode) + String(firstPart) + String(secPart);  //Number stored in variable converted to string
+console.log(phoneNo);  //Display of variable in console
