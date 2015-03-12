8bd161d622c698b7af4178f2552fa52e9753de23
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-11 22:32:17 -0400

    String conversion
    Conversion of string stored in variable with Number()

--- Video_Activities/Casting/js/script.js
+++ Video_Activities/Casting/js/script.js
@@ -1,4 +1,9 @@
 /**
  * Created by CoreyDavis on 3/11/15.
  */
-//
+//Casting
+
+var stringVar = "6";
+var result = 7 + Number(stringVar);   //Variable declaration, string conversion to number value
+console.log(result);
+
