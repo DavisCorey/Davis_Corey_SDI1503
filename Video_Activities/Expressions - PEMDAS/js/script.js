40032d3f19f58ae671c4dde2935636cb529bb006
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-11 21:58:03 -0400

    Variable Average
    4 variables declared added together and divided by the number of items

--- Video_Activities/Expressions - PEMDAS/js/script.js
+++ Video_Activities/Expressions - PEMDAS/js/script.js
@@ -1,4 +1,14 @@
 /**
  * Created by CoreyDavis on 3/11/15.
  */
-//
+//Expressions - PEMDAS - The order of Operations
+
+var quiz1 = 87;  //Variable declaration and assignment
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;  // Result Variable declared, variables added together
+console.log(average);
