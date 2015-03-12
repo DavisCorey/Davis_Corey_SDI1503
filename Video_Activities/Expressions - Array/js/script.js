8a73f795eb21afe03dc79c507ac5d5c75cda12dc
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-11 22:11:52 -0400

    Array Expression
    Declaring array and retrieving values of individual array element.

--- Video_Activities/Expressions - Array/js/script.js
+++ Video_Activities/Expressions - Array/js/script.js
@@ -1,4 +1,10 @@
 /**
  * Created by CoreyDavis on 3/11/15.
  */
-//
+//Expressions - Expressions with Arrays
+
+var orangeBins = [234, 567, 883];  // Variable declaration, assignment of array values
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];  
+console.log(total);
+
