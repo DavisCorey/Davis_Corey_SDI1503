4c10debffcf9352ac7683efae58fddaf9462efa9
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-11 22:19:22 -0400

    Concatenating Strings
    Expression - Concatenating Strings

--- Video_Activities/Expressions - Array/js/script.js
+++ Video_Activities/Expressions - Array/js/script.js
@@ -5,6 +5,18 @@
 
 var orangeBins = [234, 567, 883];  // Variable declaration, assignment of array values
 
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];  
-console.log(total);
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+//console.log(total);
 
+//Expression - Concatenating Strings
+
+var firstName = "Kermit";
+var lastName = "The Frog";
+var fullName = firstName + " " + lastName;
+
+//console.log(fullName);
+
+var a = "6";
+var b = "7";
+var result = a + b;
+console.log(result);
