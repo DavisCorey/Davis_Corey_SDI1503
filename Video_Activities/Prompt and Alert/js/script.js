4d0046a89467d21171e4669265f210b1a914edc3
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-11 23:33:05 -0400

    Prompt for Variables
    Width and Height asked for with 2 prompts

--- Video_Activities/Prompt and Alert/js/script.js
+++ Video_Activities/Prompt and Alert/js/script.js
@@ -3,9 +3,12 @@
  */
 //Prompt and Alert
 
-var userInput = prompt("Enter your year of birth:");  //Prompt for user input, store user input in variable userInput
-console.log(userInput); //Variable displayed in console that user input from prompt
+//var userInput = prompt("Enter your year of birth:");  //Prompt for user input, store user input in variable userInput
+//console.log(userInput); //Variable displayed in console that user input from prompt
 
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");  //Prompt for width
+var height = prompt("Please enter the height.");  //Prompt for height
 
 
 
