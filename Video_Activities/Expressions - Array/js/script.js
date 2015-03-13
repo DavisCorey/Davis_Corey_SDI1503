22ded701e0f39125aceaa8660f65bc23f9be9c06
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-12 23:32:52 -0400

    Array Variable
    Variable stored within array.

--- Video_Activities/Expressions - Array/js/script.js
+++ Video_Activities/Expressions - Array/js/script.js
@@ -2,10 +2,10 @@
  * Created by CoreyDavis on 3/11/15.
  */
 //Expressions - Expressions with Arrays
+var bonsai = 100;
+var orangeBins = [234, 567, 883, bonsai];  // Variable declaration, assignment of array values
 
-var orangeBins = [234, 567, 883];  // Variable declaration, assignment of array values
-
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2] + orangeBins[3];
 //console.log(total);
 
 //Expression - Concatenating Strings
