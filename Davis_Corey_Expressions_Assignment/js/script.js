55e850d92fe049cf97610f810fd5eb0dd090628d
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-12 22:02:24 -0400

    User Prompt
    Creation of user prompt for variable storage.

--- Davis_Corey_Expressions_Assignment/js/script.js
+++ Davis_Corey_Expressions_Assignment/js/script.js
@@ -7,5 +7,10 @@ var bonsaiTree;  //Variable Declaration
 var cherryBlossom;  //Variable Declaration
 var roseBush;  //Variable Declaration
 
-
+//Prompt for number of bonsai trees
+bonsaiTree = prompt("Calculation of area needed for new garden. \nPlease enter the number of Bonsia trees wanted");
+//Prompt for number of cherry blossom trees
+cherryBlossom = prompt("Please enter the number of Cherry Blossom trees wanted.");
+//Prompt for number of rose bushes
+roseBush = prompt("Please enter the number of Rose Bushes wanted.");
 
