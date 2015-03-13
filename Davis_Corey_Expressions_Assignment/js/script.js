ac8a08fe1ddb663e1b58beb11a76ea143647eba3
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-12 22:45:43 -0400

    Calculation of User Input
    Calculation of user input to calculate each plants area used.

--- Davis_Corey_Expressions_Assignment/js/script.js
+++ Davis_Corey_Expressions_Assignment/js/script.js
@@ -14,3 +14,14 @@ cherryBlossom = prompt("Please enter the number of Cherry Blossom trees wanted."
 //Prompt for number of rose bushes
 roseBush = prompt("Please enter the number of Rose Bushes wanted.");
 
+//Calculation of User input
+bonsaiArea =  bonsaiTree * 2; //  Assignment operator to calculate bonsai tree area sq. ft.
+cherryArea =  cherryBlossom * 4; // Assignment operator to calculate cherry blossom area sq. ft.
+roseArea =  roseBush * 6; //Assignment operator to calculate rose bushes sq. ft.
+
+
+
+//var treeType = [bonsaiTree, cherryBlossom, roseBush];  //Variable declaration and assignment of array values
+//var totalTrees = treeType[0] + treeType[1] + treeType[2]; //Total number of trees
+
+//console.log(bonsaiArea)
