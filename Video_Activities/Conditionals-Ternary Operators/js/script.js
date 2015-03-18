ccd224889ecd0b39a98beb4288fd3e379cc7f09f
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 19:39:00 -0400

    Ternary Operator
    Conversion to Ternary operator.

--- Video_Activities/Conditionals-Ternary Operators/js/script.js
+++ Video_Activities/Conditionals-Ternary Operators/js/script.js
@@ -12,11 +12,12 @@
 var gpa = 48
 //conditional
 //if gpa is over the min 2.0, the student can graduate
-if(gpa > 2.0){
+/*if(gpa > 2.0){
     console.log("You can graduate");
 }else{
 console.log("GPA is too low! ");
-}
+}*/
+(gpa >2.0) ? console.log("You can graduate") : console.log("GPA too low!");
 
 
 //prompts
