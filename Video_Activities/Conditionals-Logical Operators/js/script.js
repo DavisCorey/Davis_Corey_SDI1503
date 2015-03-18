9dfa5fb705df52ff912a9869f41e84ed1e6addd0
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 19:12:15 -0400

    Conditional Logic - Logical Operators
    Conditional Logic - Logical Operators

---
+++ Video_Activities/Conditionals-Logical Operators/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by CoreyDavis on 3/18/15.
+ * MDV 2330
+ *
+ */
+//Conditional Logic - Logical Operators
+
+//Add alert to test my file
+//alert("JavaScript is functional!");
+
+//variables
+
+
+//conditional
+
+
+//prompts
+
+
+//outputs
