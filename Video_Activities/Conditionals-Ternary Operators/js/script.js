acaa6e11226d7dd9db096d42e702781abdba13fb
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 19:37:05 -0400

    If else condition
    if else condition

--- Video_Activities/Conditionals-Ternary Operators/js/script.js
+++ Video_Activities/Conditionals-Ternary Operators/js/script.js
@@ -9,8 +9,15 @@
 //alert("JavaScript is functional!");
 
 //variables
-
+var gpa = 48
 //conditional
+//if gpa is over the min 2.0, the student can graduate
+if(gpa > 2.0){
+    console.log("You can graduate");
+}else{
+console.log("GPA is too low! ");
+}
+
 
 //prompts
 
