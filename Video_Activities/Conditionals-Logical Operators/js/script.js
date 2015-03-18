f508c0a61dc544521a59a968c4458735db186446
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 19:20:00 -0400

    2 Decisions
    Paycheck over 300 and phone is less than our budget. Problem is 2
    separate decisions are made and returned rather than 1.

--- Video_Activities/Conditionals-Logical Operators/js/script.js
+++ Video_Activities/Conditionals-Logical Operators/js/script.js
@@ -9,11 +9,23 @@
 //alert("JavaScript is functional!");
 
 //variables
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
 
 //conditional
-
-
+//if the price of the phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget){
+    console.log("We can buy the phone! ");
+}else{
+    console.log("No phone for you! ");
+}
+if(paycheck >300){
+    console.log("We can buy the phone! ");
+}else{
+    console.log("No phone for you! ");
+}
 //prompts
 
 
