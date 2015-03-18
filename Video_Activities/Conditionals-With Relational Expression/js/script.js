9a523f427632dd217b245621c3278630454b817b
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 18:21:22 -0400

    Conditionals with RE
    Relational Expression created by refactoring Conditionals directory.

---
+++ Video_Activities/Conditionals-With Relational Expression/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by CoreyDavis on 3/18/15.
+ * MDV 2330
+ *
+ */
+//Conditional Logic - Relational Expressions
+
+//Add alert to test my file
+//alert("JavaScript is functional!");
+
+//variables
+var oldEnough = true;
+
+//if the child is old enough, then print to the console "you can ride!"
+
+//conditional
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+//prompts
+
+
+//outputs
