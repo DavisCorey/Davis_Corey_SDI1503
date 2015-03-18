33f350c833e3ef9acd2e3e1810881bbc3585ace0
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 18:15:05 -0400

    If conditional
    if true print string.

--- Video_Activities/Conditionals/js/script.js
+++ Video_Activities/Conditionals/js/script.js
@@ -3,13 +3,21 @@
  * MDV 2330
  *
  */
+//Conditional Logic
 
 //Add alert to test my file
 //alert("JavaScript is functional!");
 
 //variables
+var oldEnough = true;
 
+//if the child is old enough, then print to the console "you can ride!"
 
+//conditional
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
 //prompts
 
 
