c2fe260124c31675122e761a1620ba6348581318
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 18:25:51 -0400

    False condition
    No output, comparison of value to requirement. This comparison equates
    to boolean value of true or false.

--- Video_Activities/Conditionals-With Relational Expression/js/script.js
+++ Video_Activities/Conditionals-With Relational Expression/js/script.js
@@ -9,12 +9,12 @@
 //alert("JavaScript is functional!");
 
 //variables
-var oldEnough = true;
+var kidHeight = 30;
 
-//if the child is old enough, then print to the console "you can ride!"
+//if the kid is over 48 inches in height additional code is executed.
 
 //conditional
-if(oldEnough){
+if(kidHeight > 48){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
