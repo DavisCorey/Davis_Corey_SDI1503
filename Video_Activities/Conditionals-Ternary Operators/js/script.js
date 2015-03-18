75a81bae5c7479390f99238ef1e2268e30ba0aa4
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 19:32:41 -0400

    File Creation
    Conditional Logic - Ternary Operators

---
+++ Video_Activities/Conditionals-Ternary Operators/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by CoreyDavis on 3/18/15.
+ * MDV 2330
+ *
+ */
+//Conditional Logic - Ternary Operators
+
+//Add alert to test my file
+//alert("JavaScript is functional!");
+
+//variables
+
+//conditional
+
+//prompts
+
+
+//outputs
