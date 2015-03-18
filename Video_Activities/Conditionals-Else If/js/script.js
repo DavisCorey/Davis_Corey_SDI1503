9deb6f3009b0daaf7c97d9916a3c0fa036841b57
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-18 18:54:20 -0400

    2nd condition added
    else if added to evaluate a 2nd condition in a if statement.

--- Video_Activities/Conditionals-Else If/js/script.js
+++ Video_Activities/Conditionals-Else If/js/script.js
@@ -11,15 +11,18 @@
 //variables
 var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
-
+var wParentHeight = 45;  //the height of the kid with the parent
 //if the kid is over 48 inches in height additional code is executed.
 
 //conditionals
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present. ");
 }else{
+    //sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first! ");
 }
 //prompts
