0f28a9cf52547d54cf34d83b58528df67fd3984c
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-19 15:45:48 -0400

    Conditional Worksheet
    Creation of html, css, js files. Link creation tested, js alert
    commented out.

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by CoreyDavis on 3/19/15.
+ * MDV 2330
+ * Conditionals_worksheet
+ */
+
+//Add alert to test my file
+//alert("JavaScript is functional!");
+
+//variables
+
+
+//prompts
+
+
+//outputs
