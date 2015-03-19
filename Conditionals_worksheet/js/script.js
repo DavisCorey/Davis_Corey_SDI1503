10223ef460ca2f92cf06d85aeca6f744a130f06c
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-19 15:57:04 -0400

    Example 1
    Variable declared for temp. Conditional set with if else.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -8,7 +8,14 @@
 //alert("JavaScript is functional!");
 
 //variables
+var temp=80;  //Hot enough example, is it hot enough to go to the beach
 
+//Conditionals
+if(temp <75){  //If less than 75 degrees
+    console.log("We will go to the beach! ");
+}else{  //If greater than 75 or equal
+    console.log("We will go to the movies.");
+}
 
 //prompts
 
