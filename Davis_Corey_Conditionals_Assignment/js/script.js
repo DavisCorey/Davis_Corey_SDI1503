59d03db199add01f98f1ccbd94efec284e77dfb1
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-19 22:20:53 -0400

    Variable Declarations
    Declaration of variables, 3 user prompts, 1 variable for calculation

--- Davis_Corey_Conditionals_Assignment/js/script.js
+++ Davis_Corey_Conditionals_Assignment/js/script.js
@@ -7,7 +7,13 @@
 //Add alert to test my file
 //alert("JavaScript is functional!");
 
-//variables
 
+//variables
+var budget=Number(prompt("Let's calculate how much it cost to feed everyone. \n Can you input your estimated budget in dollars.",12000));
+var meeting=Number(prompt("How many meetings should we schedule this year?",12));
+var attendees=Number(prompt("Please input the number of people the venue can hold",50));
+var personCost;  //Variable Declaration, hold calculated cost to feed each person
+
+//
 
 //outputs
