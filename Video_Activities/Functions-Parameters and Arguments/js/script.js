e2a00f5c0c8dbfcf91fe05f9c60ccdd73f4ede58
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-26 21:40:16 -0400

    Function Refactor
    Move to Parameters and Arguments

---
+++ Video_Activities/Functions-Parameters and Arguments/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by CoreyDavis on 3/26/15.
+ * MDV 2330
+ * Functions - Parameters and Arguments
+ */
+
+//Add alert to test my file
+alert("JavaScript is functional!");
+
+//variables
+
+
+//functions
+
+
+//outputs
