7fbb6de777f386e2d0e520cf3c4d87511dbcbdc6
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-26 21:24:19 -0400

    Functions File creation

---
+++ Video_Activities/Functions/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by CoreyDavis on 3/26/15.
+ * MDV 2330
+ * Functions - Basic Structure
+ */
+
+//Add alert to test my file
+//alert("JavaScript is functional!");
+
+//variables
+
+
+//functions
+
+
+//outputs
