2f7aa5dea4f8bd55a3bc7d75b2d004b5d744a62d
Corey Davis <coreyjamesdavis@gmail.com>
2015-03-26 21:34:47 -0400

    Run function
    calcArea function run

--- Video_Activities/Functions/js/script.js
+++ Video_Activities/Functions/js/script.js
@@ -8,16 +8,18 @@
 //alert("JavaScript is functional!");
 
 //variables
-
+var width=5;
 
 //functions
 function outptMsg(){  //Function definition
     console.log("Hello World");  //Output string
 }
 function calcArea(){  //Function definition
-    var width=20;
-    var height=30;
-    var area=width*height;
+    var width=20;  //Set width
+    var height=30;  //Set height
+    var area=width*height;  //Calculate and store in area variable
     console.log(area);  //Output calculation of area
 }
 //outputs
+calcArea();  //Run calcArea function
+console.log(width);
